38638af4ea8be2ef7fac2e905aae9deb399fc954
abaym <abaym.kz@gmail.com>
2023-01-02 16:12:26 +0600

    End of Section 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}
