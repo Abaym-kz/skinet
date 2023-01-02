114f744e95903598efdd82f2b31f575617d78673
abaym <abaym.kz@gmail.com>
2023-01-02 08:17:23 +0600

    Initial Commit

---
+++ API/Startup.cs
@@ -0,0 +1,45 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(
+                x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
