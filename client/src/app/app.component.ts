cf5ff0e654c20261af2c38d79f3e7b43e85ec44e
abaym <abaym.kz@gmail.com>
2023-01-13 19:17:05 +0600

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: IPagination) =>
+    {
+      this.products = response.data;
+      console.log(response);
+    }, error =>
+    {
+      console.log(error);
+    });
+  }
 }
